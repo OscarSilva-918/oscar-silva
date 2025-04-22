5376bf34b18cd8d3c3dcafef906cb889c16de77c
oscar <devoscarsilva@gmail.com>
2025-04-21 22:30:08 -0300

    Agregue enlaces a mis proyectos

--- src/App.tsx
+++ src/App.tsx
@@ -198,7 +198,7 @@ function App() {
                   Sistema de Motomandados
                 </h3>
               </div>
-              <a href="https://dashboard-demo-1-0.vercel.app/dashboard">
+              <a href="https://dashboard-demo-1-0.vercel.app">
                 <div className="mb-4 overflow-hidden rounded-lg">
                   <img
                     src="https://images.unsplash.com/photo-1526367790999-0150786686a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80"
@@ -232,7 +232,7 @@ function App() {
                 />
                 <h3 className="text-xl font-semibold">Proyecto Ecommerce</h3>
               </div>
-              <a href="">
+              <a href="https://misiomarket-app.vercel.app/">
               <div className="mb-4 overflow-hidden rounded-lg">
                 <img
                   src="https://media.istockphoto.com/id/1428709516/es/foto/compras-en-l%C3%ADnea-mujer-mano-compras-en-l%C3%ADnea-en-computadora-port%C3%A1til-con-diagrama-de-icono.jpg?s=1024x1024&w=is&k=20&c=0tc26isn1iwBi9Iy3G1RulocAmCZ8dFFru8tisqE-yg="
